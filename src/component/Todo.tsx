3798a0a4e2af9560d81b86607e385a71a7daea41
mac <mac@iMac.local>
2022-09-17 10:27:35 +0530

    useRef

--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Todo = () => {
-  const [todo, setTodo] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   const [todoList, setTodoList] = useState<string[]>([]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
-  };
-
-
   const addTodo = () => {
-    setTodoList([...todoList, todo]);
-    setTodo("");
-}
+    if (inputRef.current) {
+      const todo = inputRef.current?.value;
+      setTodoList([...todoList, todo]);
+    }
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="input Value"
-        value={todo}
-        onChange={handleChange}
-      />
+      <input ref={inputRef} type="text" placeholder="input Value" />
       <button onClick={addTodo}>Add</button>
       <div>
         {todoList.map((cv) => (
